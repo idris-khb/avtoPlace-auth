b639affe380d653bc97627f4b9088875e2245caa
idris <idris-rr@mail.ru>
2025-12-19 14:50:42 +0500

    first commit

---
+++ src/main/java/com/avtoplace/auth/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.avtoplace.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/health").permitAll()
+                                               .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 ->oauth2.jwt(jwtConfigurer -> {}));
+
+        return http.build();
+    }
+}
